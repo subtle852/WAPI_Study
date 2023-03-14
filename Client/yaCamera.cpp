ac96c0e43261d3b821248a6445df8207807fc90b
subtle852 <subtle852@gmail.com>
2023-03-15 07:25:07 +0900

    CameraEffect

--- Client/yaCamera.cpp
+++ Client/yaCamera.cpp
@@ -4,6 +4,7 @@
 #include "yaTransform.h"
 #include "yaInput.h"
 #include "yaTime.h"
+#include "yaImage.h"
 
 extern ya::Application application;
 namespace ya
@@ -13,11 +14,20 @@ namespace ya
 	Vector2 Camera::mDistance = Vector2::Zero;
 	GameObject* Camera::mTarget = nullptr;
 
+	Camera::eCameraEffectType Camera::mType = Camera::eCameraEffectType::None;
+	class Image* Camera::mCutton = nullptr;
+	float Camera::mCuttonAlpha = 1.0f;
+	float Camera::mAlphaTime = 0.0f;
+	float Camera::mEndTime = 5.0f;
+
 	void Camera::Initiailize()
 	{
 		mResolution.x = application.GetWidth();
 		mResolution.y = application.GetHeight();
 		mLookPosition = (mResolution / 2.0f);
+
+		mType = eCameraEffectType::FadeIn;
+		mCutton = Image::Create(L"Cutton", mResolution.x, mResolution.y, RGB(0, 0, 0));
 	}
 
 	void Camera::Update()
@@ -42,8 +52,50 @@ namespace ya
 				= mTarget->GetComponent<Transform>()->GetPos();
 		}
 
+
+		if (mAlphaTime < mEndTime)
+		{
+			//255 - > 1
+			mAlphaTime += Time::DeltaTime();
+			float ratio = (mAlphaTime / mEndTime);
+
+			if (mType == eCameraEffectType::FadeIn)
+			{
+				mCuttonAlpha = 1.0f - ratio;
+			}
+			else if (mType == eCameraEffectType::FadeOut)
+			{
+				mCuttonAlpha = ratio;
+			}
+			else
+			{
+
+			}
+		}
+
 		mDistance = mLookPosition - (mResolution / 2.0f);
 	}
+
+	void Camera::Render(HDC hdc)
+	{
+		if (mAlphaTime < mEndTime 
+			&& mType == eCameraEffectType::FadeIn)
+		{
+			BLENDFUNCTION func = {};
+			func.BlendOp = AC_SRC_OVER;
+			func.BlendFlags = 0;
+			func.AlphaFormat = 0;
+			func.SourceConstantAlpha = (BYTE)(255.0f * mCuttonAlpha);
+
+			AlphaBlend(hdc, 0, 0
+				, mResolution.x, mResolution.y
+				, mCutton->GetHdc()
+				, 0, 0
+				, mCutton->GetWidth(), mCutton->GetHeight()
+				, func);
+		}
+	}
+
 	void Camera::Clear()
 	{
 		mResolution.x = application.GetWidth();
--- Client/yaCamera.h
+++ Client/yaCamera.h
@@ -7,8 +7,18 @@ namespace ya
 	class Camera
 	{
 	public:
+		enum class eCameraEffectType
+		{
+			None,
+			FadeIn,
+			FadeOut,
+			//Shake,
+			End,
+		};
+
 		static void Initiailize();
 		static void Update();
+		static void Render(HDC hdc);
 		static void Clear();
 
 		static void SetTarget(GameObject* target) { mTarget = target; }
@@ -20,5 +30,12 @@ namespace ya
 		static Vector2 mLookPosition;
 		static Vector2 mDistance;
 		static GameObject* mTarget;
+
+		static eCameraEffectType mType;
+		static class Image* mCutton;
+		static float mCuttonAlpha;
+		static float mAlphaTime;
+		static float mEndTime;
+
 	};
 }
